8394b756264a6f7eed0da61d38ac8621fcfa9919
Ricardo Terán <39249155+Rikardo816@users.noreply.github.com>
2025-04-15 21:50:39 -0500

    fix: worflow again x2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: `https://Rikardo816.github.io/landing-app`,
-    output: "static",
-    outDir: "./docs",
-    build: {
-        assets: "astro",
-    },
+    site: 'https://Rikardo816.github.io',
+    base: 'landing-app',
 });
