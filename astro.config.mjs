529935eecf2112c8fdbff7e4128d833481872536
Ricardo Terán <39249155+Rikardo816@users.noreply.github.com>
2025-04-15 21:34:08 -0500

    add workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://Rikardo816.github.io',
+    base: 'landing-app'
+});
